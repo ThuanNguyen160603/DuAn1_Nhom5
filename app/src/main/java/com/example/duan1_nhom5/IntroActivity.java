3df14484e333f8c7fa5c26839969af8e9f0b4a93
ThuanNguyen160603 <96214401+ThuanNguyen160603@users.noreply.github.com>
2022-11-14 10:14:19 +0700

    vd1

---
+++ app/src/main/java/com/example/duan1_nhom5/IntroActivity.java
@@ -0,0 +1,14 @@
+package com.example.duan1_nhom5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
