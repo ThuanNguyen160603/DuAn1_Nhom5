ae27a508bde464f91a8bde0ba9755be0cc8e64df
wizacne <117801856+wizacne@users.noreply.github.com>
2022-11-15 22:24:46 +0700

    LoginGoogle

--- app/src/main/java/com/example/duan1_nhom5/IntroActivity.java
+++ app/src/main/java/com/example/duan1_nhom5/IntroActivity.java
@@ -2,7 +2,12 @@ package com.example.duan1_nhom5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        ImageView ivLogo = findViewById(R.id.iv_logo);
+        Glide.with(this).load(R.drawable.logo_intro).into(ivLogo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        },4000);
     }
 }
