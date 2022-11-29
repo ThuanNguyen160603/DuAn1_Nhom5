347bf87f30f30c0187cf3599595e1ca6f27bca26
n3kofpt <117456924+n3kofpt@users.noreply.github.com>
2022-11-30 01:52:15 +0700

    MainScreenActivity
    Cái này tui tạo ra theo deadline, anh em chỉnh lại nhé

---
+++ app/src/main/java/com/example/duan1_nhom5/MainScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.duan1_nhom5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
